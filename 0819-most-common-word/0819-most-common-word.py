a5faa27b7cc111a0429d4a8990d834997820580c
ngothitho2021-byte <ngothitho2021@gmail.com>
2026-03-09 23:12:18 +0700

    Time: 3 ms (73.14%), Space: 12.5 MB (60.38%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,16 @@
+import re
+from collections import Counter
+
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        words = re.findall(r'\w+', paragraph.lower())
+        banned_set = set(banned)
+        counts = Counter(word for word in words if word not in banned_set)
+        return counts.most_common(1)[0][0]
+      
+        
