ae90867e832ab8d9894ff8941baecb6f9f649d75
ngothitho2021-byte <ngothitho2021@gmail.com>
2026-03-24 00:15:23 +0700

    Time: 0 ms (100%), Space: 12.3 MB (78.23%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -5,14 +5,18 @@ class Solution(object):
         :rtype: int
         """
        
-        count = {}
+        
+        count = {}   # Dictionary dùng để lưu số lần xuất hiện của từng phần tử
+    # Duyệt qua từng phần tử trong mảng nums
         for i in nums:
-            if i not in count:
+            if i not in count:   # Nếu phần tử chưa có trong dictionary thì khởi tạo
                 count[i] = 0
-            count[i] += 1
+            count[i] += 1        # Tăng số lần xuất hiện của phần tử đó lên 1
 
-        total = 0
+        total = 0   # Biến lưu tổng các phần tử duy nhất
+    # Duyệt qua từng cặp (num, freq) trong dictionary
         for num, freq in count.items():
-            if freq == 1:      # chỉ cộng nếu xuất hiện đúng 1 lần
-             total += num
-        return total
+            if freq == 1:        # Nếu phần tử chỉ xuất hiện đúng 1 lần
+                total += num     # Cộng giá trị của phần tử đó vào tổng
+
+        return total  # Trả về kết quả cuối cùng
